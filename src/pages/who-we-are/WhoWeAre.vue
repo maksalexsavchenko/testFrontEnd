e8ea46ad3700e891cc84baa07deb86a7412e5586
callejon1021 <andreikrz1021@gmail.com>
2023-01-27 00:22:35 +0200

    fix html/scss cards

--- src/pages/who-we-are/WhoWeAre.vue
+++ src/pages/who-we-are/WhoWeAre.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="who-we-are">
     <hero-block/>
+    <features-block/>
   </div>
 </template>
 
 <script>
 import HeroBlock from "@/pages/who-we-are/partials/HeroBlock.vue";
+import FeaturesBlock from "@/pages/who-we-are/partials/FeaturesBlock";
 
 export default {
   name: "WhoWeAre",
-  components: {HeroBlock}
+  components: {FeaturesBlock, HeroBlock}
 }
 </script>
 
--- src/pages/who-we-are/partials/FeaturesBlock.vue
+++ src/pages/who-we-are/partials/FeaturesBlock.vue
@@ -1,7 +1,25 @@
 <template>
-  <div class="features">
+  <div class="features-block">
     <div class="container">
-      test features
+      <div class="block-holder">
+        <div class="features-block_text">
+          We are <span>technologies </span>
+          <div class="features-block_text--item">
+            Our team was the first on the market to develop and implement unique multi-channel exchange capabilities
+            between CEX and DEX with calculation of all possible details. Our users do not need to manually conduct
+            numerous exchanges to buy, sell, trade and invest tokens that have not yet listed on major CEX or are going
+            to stay exclusively on DEX platforms, as proven Swapuz algorithms have come to the rescue to save your
+            precious time and simplify this process.
+          </div>
+        </div>
+        <div class="features-block-image">
+          <img
+              srcset="../../../assets/images/png/1features-image1x.png 1x,
+               ../../../assets/images/png/1features-image2x.png 2x,
+               ../../../assets/images/png/1features-image4x.png 4x"
+              src="../../../assets/images/png/1features-image2x.png" alt="features-image">
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -12,6 +30,6 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss">@import "../../../assets/styles/pages/who-we-are/partials/features-block.scss";
 
 </style>

c39b8d962f28c9f5367727a6f379e84c4af00f8f
callejon1021 <andreikrz1021@gmail.com>
2023-01-27 21:30:46 +0200

    fix svg image

--- src/pages/who-we-are/partials/PartnersBlock.vue
+++ src/pages/who-we-are/partials/PartnersBlock.vue
@@ -7,47 +7,19 @@
 
       <div class="partners-block_image">
         <div class="partners-block_image_img">
-          <img
-              srcset="../../../assets/images/png/coinmarketcap-img1x.png 1x,
-               ../../../assets/images/png/coinmarketcap-img2x.png 2x,
-               ../../../assets/images/png/coinmarketcap-img4x.png 4x"
-              src="../../../assets/images/png/coinmarketcap-img1x.png" alt="coinmarketcap-image">
+          <img src="../../../assets/images/svg/coinMarketCap-img.svg" alt="coinmarketcap-image">
 
-          <img
-              srcset="../../../assets/images/png/binance-img1x.png 1x,
-               ../../../assets/images/png/binance-img2x.png 2x,
-               ../../../assets/images/png/binance-img4x.png 4x"
-              src="../../../assets/images/png/binance-img2x.png" alt="binance-image">
+          <img src="../../../assets/images/svg/binance-img.svg" alt="binance-image">
 
-          <img
-              srcset="../../../assets/images/png/coinbase-img1x.png 1x,
-               ../../../assets/images/png/coinbase-img2x.png 2x,
-               ../../../assets/images/png/coinbase-img4x.png 4x"
-              src="../../../assets/images/png/coinbase-img2x.png" alt="coinbase-image">
+          <img src="../../../assets/images/svg/coinbase-img.svg" alt="coinbase-image">
 
-          <img
-              srcset="../../../assets/images/png/ferrari-img1x.png 1x,
-               ../../../assets/images/png/ferrari-img2x.png 2x,
-               ../../../assets/images/png/ferrari-img4x.png 4x"
-              src="../../../assets/images/png/ferrari-img2x.png" alt="ferrari-image">
+          <img src="../../../assets/images/svg/ferrari-img.svg" alt="ferrari-image">
 
-          <img
-              srcset="../../../assets/images/png/coinbase-img1x.png 1x,
-               ../../../assets/images/png/coinbase-img2x.png 2x,
-               ../../../assets/images/png/coinbase-img4x.png 4x"
-              src="../../../assets/images/png/coinbase-img2x.png" alt="coinbase-image">
+          <img src="../../../assets/images/svg/coinbase-img.svg" alt="coinbase-image">
 
-          <img
-              srcset="../../../assets/images/png/superdry-img1x.png 1x,
-               ../../../assets/images/png/superdry-img2x.png 2x,
-               ../../../assets/images/png/superdry-img4x.png 4x"
-              src="../../../assets/images/png/superdry-img2x.png" alt="superdry-image">
+          <img src="../../../assets/images/svg/superdry-img.svg" alt="superdry-image">
 
-          <img
-              srcset="../../../assets/images/png/binance-img1x.png 1x,
-               ../../../assets/images/png/binance-img2x.png 2x,
-               ../../../assets/images/png/binance-img4x.png 4x"
-              src="../../../assets/images/png/binance-img2x.png" alt="binance-image">
+          <img src="../../../assets/images/svg/binance-img.svg" alt="binance-image">
         </div>
       </div>
     </div>
